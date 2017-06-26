d12ac3be4727ce8d7e4c6cca4a198ec9126c3731
simurashka <simurashka@gmail.com>
2017-06-27 00:11:40 +0300

    Homework1

---
+++ GoJavaTechSkills/src/Module1/Homework1.java
@@ -0,0 +1,11 @@
+package Module1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("How I am going to spend summer of 2017");
+
+        System.out.println("Learning Java, obviously!");
+    }
+}
