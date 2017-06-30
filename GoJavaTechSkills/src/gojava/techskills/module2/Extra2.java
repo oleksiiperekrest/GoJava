8220170e489264f0d9617abb6d2a674feaa3d2a5
simurashka <simurashka@gmail.com>
2017-06-30 14:58:20 +0300

    finished extra 2 for module 2

--- GoJavaTechSkills/src/gojava/techskills/module2/Extra2.java
+++ GoJavaTechSkills/src/gojava/techskills/module2/Extra2.java
@@ -7,8 +7,42 @@ package gojava.techskills.module2;
 каком этаже, какого подъезда расположенна эта квартира. Если такой
 квартиры нет в этом доме то нужно сообщить об этом пользователю.
  */
+
+import static gojava.techskills.module2.Homework2.getIntInput;
+
 public class Extra2 {
     public static void main(String[] args) {
+
+        int storeys         = 9;
+        int blocks          = 4;
+        int flatsOnStorey   = 4;
+
+        System.out.println("This building has " + storeys + " storeys, " + blocks + " blocks with " +
+                storeys * flatsOnStorey + " flats in each (" + flatsOnStorey + " flats on storey, " +
+                storeys * blocks * flatsOnStorey + " flats in total).");
+        System.out.print("Enter a flat number to find: ");
+        int flat = getIntInput();
+
+
+        checkFlat(storeys, blocks, flatsOnStorey, flat);
+
+    }
+    static void checkFlat(int storeys, int blocks, int flatsOnStorey, int flat){
+        int totalFlats = storeys * blocks * flatsOnStorey;
+
+        if (flat < 1 || flat > totalFlats) {
+            System.out.println("Sorry, a flat with such number is not present in this building.");
+        }
+        else {
+            int block = 1;
+            while (flat > storeys * flatsOnStorey * block) block++;
+
+            int storey = (flat - (storeys * flatsOnStorey * (block-1)) + (flatsOnStorey-1)) / flatsOnStorey;
+
+            System.out.println("\nThe requested flat is in block " + block + " on floor " + storey + ".");
+
+        }
+
 
     }
 }
