9e63c2b4e8a810f75cc1271e85aa2b0dcd306e5d
simurashka <simurashka@gmail.com>
2017-06-30 23:51:44 +0300

    created arithmetic progression methods

--- GoJavaTechSkills/src/gojava/techskills/module2/Progressions.java
+++ GoJavaTechSkills/src/gojava/techskills/module2/Progressions.java
@@ -15,10 +15,29 @@ package gojava.techskills.module2;
 public class Progressions {
     public static void main(String[] args) {
 
-    }
-    
-    static boolean checkArithmeticProgression(int[] array) {
+        int[] array = {1,4,7,10,13};
 
-        return false;
+        System.out.println(array[0]);
+        System.out.println(checkArithmeticProgression(array));
+        System.out.println(getArithmeticStep(array));
+    }
+
+    static int getArithmeticStep(int[] array) {
+        if(checkArithmeticProgression(array)) return array[1] - array[0];
+        else return 0;
+    }
+
+    static boolean checkArithmeticProgression(int[] array) {
+        if (array.length < 2){
+            System.out.println("Entered progression is too small for analysis");
+            return false;
+        }
+        int step = array[1] - array[0];
+        for (int i = 2; i < array.length; i++) {
+            if (step != array[i] - array[i-1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
