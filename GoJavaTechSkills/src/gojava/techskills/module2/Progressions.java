7623bfaedf5760bf53e5c9e728fbd8cbd30d0586
simurashka <simurashka@gmail.com>
2017-07-03 19:33:47 +0300

    ArrayList not needed

--- GoJavaTechSkills/src/gojava/techskills/module2/Progressions.java
+++ GoJavaTechSkills/src/gojava/techskills/module2/Progressions.java
@@ -12,8 +12,6 @@ package gojava.techskills.module2;
 строку 0,2,4,6,8,10,12 ответом программы должно быть число 14.
  */
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
