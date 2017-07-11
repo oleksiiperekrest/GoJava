cd289f75d88443d64770ba9627427373f5257654
simurashka <simurashka@gmail.com>
2017-07-12 00:45:37 +0300

    getPositiveIntInput()

--- GoJavaTechSkills/src/gojava/techskills/methods.java
+++ GoJavaTechSkills/src/gojava/techskills/methods.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
  * Here will lie all the everyday-useful methods.
  */
 public class methods {
+
+    /**
+     * @return An integer input from the user.
+     */
     public static int getIntInput() {
         Scanner input = new Scanner(System.in);
         while (!input.hasNextInt()) {
@@ -13,4 +17,29 @@ public class methods {
             input.next();
         }
         return input.nextInt();
+    }
+
+    /**
+     * Gets user input of a positive integer.
+     *
+     * @param message Message to be displayed to invite input.
+     * @return Positive integer entered by user.
+     */
+    public static int getPositiveIntInput(String message) {
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String line = input.nextLine();
+            try {
+                int n = Integer.parseInt(line);
+                if (n > 0) {
+                    return n;
+                }
+            } catch (NumberFormatException e) {
+
+            }
+            System.out.print("Please enter a positive whole number!\n" + message);
+        }
+    }
 }
