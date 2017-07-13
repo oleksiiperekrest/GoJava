e6abc584d4750fc482d2f5cb55ad0bdf1963d598
simurashka <simurashka@gmail.com>
2017-07-13 23:27:31 +0300

    cool new inputs: int with message, float, int and float arrays

--- GoJavaTechSkills/src/gojava/techskills/methods.java
+++ GoJavaTechSkills/src/gojava/techskills/methods.java
@@ -20,6 +20,19 @@ public class methods {
     }
 
     /**
+     * @return An integer input from the user.
+     */
+    public static int getIntInput(String message) {
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+        while (!input.hasNextInt()) {
+            System.out.println("(Enter a whole number!)");
+            input.next();
+        }
+        return input.nextInt();
+    }
+
+    /**
      * Gets user input of a positive integer.
      *
      * @param message Message to be displayed to invite input.
@@ -41,5 +54,64 @@ public class methods {
             }
             System.out.print("(Enter a positive whole number!)\n" + message);
         }
+    }
+
+    /**
+     * @return An float number input by user.
+     */
+    public static float getFloatInput() {
+        Scanner input = new Scanner(System.in);
+        while (!input.hasNextFloat()) {
+            System.out.println("(Enter a number!)");
+            input.next();
+        }
+        return input.nextFloat();
+    }
+
+    /**
+     *
+     * @param message Message shown to user to invite input.
+     * @return An float number input by user.
+     */
+    public static float getFloatInput(String message) {
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+        while (!input.hasNextFloat()) {
+            System.out.println("(Enter a number!)");
+            input.next();
+        }
+        return input.nextFloat();
+    }
+
+    /**
+     *
+     * @param length Predetermined length of array.
+     * @param message Message shown to user to invite input.
+     * @return Array of integers entered by user.
+     */
+    public static int[] getIntsArrayInput(int length, String message) {
+        System.out.println(message);
+        int[] array = new int[length];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getIntInput("Enter integer number (" + (i+1) + " of " + length + ").");
+        }
+        return array;
+    }
+
+    /**
+     *
+     * @param length Predetermined length of array.
+     * @param message Message shown to user to invite input.
+     * @return Array of floats entered by user.
+     */
+    public static float[] getFloatsArrayInput(int length, String message) {
+        System.out.println(message);
+        float[] array = new float[length];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getFloatInput("Enter a number (" + (i+1) + " of " + length + ").");
+        }
+        return array;
     }
 }
