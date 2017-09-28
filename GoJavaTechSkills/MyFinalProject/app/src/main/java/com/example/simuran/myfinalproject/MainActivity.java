47517f9c3b0bb6ddc1c0fec8abdae87e533fc236
simurashka <simurashka@gmail.com>
2017-09-28 22:10:15 +0300

    tt

---
+++ GoJavaTechSkills/MyFinalProject/app/src/main/java/com/example/simuran/myfinalproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.simuran.myfinalproject;
+
+import android.support.v7.app.*;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
